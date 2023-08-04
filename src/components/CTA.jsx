5cbb498464f9177e5c3d73437308593bdaf3611b
unknown <patrick.doberiner@gmail.com>
2023-08-04 16:13:54 +0100

    Add the downloadable cv

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import CV from "../assets/CV.pdf";
 const CTA = () => {
   return (
     <div className="cta">
-      <a className="btn" href="#cv" data-aos="zoom-in-left" download>
+      <a className="btn" href={CV} data-aos="zoom-in-left" download="CV Essama.pdf">
         Download CV
       </a>
       <a className="btn btn-primary" href="#contact" data-aos="zoom-in-right">
